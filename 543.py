6d1e0f7b1d464dceab2b0a2da59a81f23db8303c
rachit976 <mukeshsoni38@gmail.com>
2025-04-20 19:52:10 +0530

    Add files via upload

---
+++ 543.py
@@ -0,0 +1,30 @@
+student_data ={'id1'}:
+{'name': ['rachit'],
+  'class': ['v'],
+  'subject_integration':['english math science'  ]
+  } ,
+
+ 'id2' :
+{,name :['rachit']
+'class':['v'] 
+'subject_interration':['english math science'  ]
+}
+
+id2' :
+{,name :['rahul']
+'class':['v'] 
+'subject_interration':['english math science'  ]
+}
+
+id2' :
+{,name} :['pari']
+'class':['v'] 
+'subject_interration':['english math science'  ]
+
+result ={}
+
+for key,value in  student_data.items():
+if not in result.values():
+ result[key]= value
+
+ print(result)
